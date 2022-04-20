1a3e22b881eac09964418fe9b70f7b77ccb7ac82
Sami Alhamad <sakosalo@outlook.com>
2022-04-20 03:52:18 +0300

    Navbar & Footer components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
+import Footer from './Components/layout/Footer'
 
 function App() {
   return (
-    <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
 
         <main>content</main>
+        <Footer />
       </div>
-    </Router>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,11 @@
 import React from 'react'
-import { FaGithub } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
 import { PropTypes } from 'prop-types'
 
 function Navbar({ title }) {
   return (
-    <nav className='navbar mb-12 shadow-lg bg-neutral text-netural-content'>
-      <div className='container msx-auto'>
-        <div className='flex-none px-2 mx-2'>
-          <FaGithub className='inline pr-2 text-4xl' />
-          <Link to='/' className='text-lg font-bold align-middle'>
-            {title}
-          </Link>
-        </div>
-        <div className='flex-2 px-2 mx-2'>
-          <div className='flex justify-end'>
-            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
-              Home
-            </Link>
-            <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
-              About
-            </Link>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <div className='navbar bg-base-100 shadow-lg'>
+      <a className='btn btn-ghost normal-case text-xl align-middle'>{title}</a>
+    </div>
   )
 }
 
